67704a1dae9f9527a62ececc9c669e899b8f769c
16gsanket <16gsanket@gmail.com>
2024-06-27 23:45:59 +0530

    added path

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Button from '../components/Button';
+import { useNavigate } from '@tanstack/react-location';
 
 const HomePage: React.FC = () => {
+  const navigate = useNavigate()
+
+  const handleLoginClick = () => {
+    navigate({to:'/login' , replace:false})
+  }
+
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-cover" style={{ backgroundImage: "url('/images/Background1.png')" }}>
       <div className="bg-black bg-opacity-50 p-8 rounded-lg text-center">
         <h1 className="text-white text-3xl mb-4">A New Era of Global Trade 4.0</h1>
-        <Button text="Login" onClick={() => { /* Navigate to login form */ }} />
+        <Button text="Login" onClick={handleLoginClick} />
         <p className="text-white mt-4">
           Not Signed Up? <a href="/signup" className="underline">Sign-up here in a click</a>
         </p>
